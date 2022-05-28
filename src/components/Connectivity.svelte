38b90114794d9b432ac6825ee8700a3c90cc1d7c
Binsar Dwi Jasuma <binsarjr121@gmail.com>
2022-05-28 09:45:10 +0700

    detect dev tools

--- src/components/Connectivity.svelte
+++ src/components/Connectivity.svelte
@@ -1,14 +1,25 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
+
 	// @ts-ignore
 	import InternetConnection from 'svelte-internet-connection';
+	let visible = false;
+	onMount(() => {
+		visible = true;
+	});
 </script>
 
-<InternetConnection let:offline>
+{#if visible}
+	<InternetConnection let:offline>
 		{#if offline}
-			<section class="fixed flex items-center justify-center top-0 left-0 bottom-0 bg-black opacity-50 right-0  font-bold px-4 py-1 text-center z-50" id="offline">
+			<section
+				class="fixed flex items-center justify-center top-0 left-0 bottom-0 bg-black opacity-50 right-0  font-bold px-4 py-1 text-center z-50"
+				id="offline"
+			>
 				<p class="text-md bg-red-500 text-white px-4 py-2 rounded">
 					Saat ini kamu sedang offline. Aplikasi hanya bisa digunakan ketika online
 				</p>
 			</section>
 		{/if}
-</InternetConnection>
+	</InternetConnection>
+{/if}
---
+++ static/devtools.js
@@ -0,0 +1,33 @@
+!function() {
+    function detectDevTool(allow) {
+      if(isNaN(+allow)) allow = 100;
+      var start = +new Date(); // Validation of built-in Object tamper prevention.
+      /*
+      Jangan pakai api dari sini bikin berat server
+      Udah saya bikinkan librarynya cek aja di https://github.com/binsarjr/apiamikomone
+      */
+      debugger;
+      var end = +new Date(); // Validates too.
+      if(isNaN(start) || isNaN(end) || end - start > allow) {
+        // input your code here when devtools detected.
+      }
+    }
+    if(window.attachEvent) {
+      if (document.readyState === "complete" || document.readyState === "interactive") {
+          detectDevTool();
+        window.attachEvent('onresize', detectDevTool);
+        window.attachEvent('onmousemove', detectDevTool);
+        window.attachEvent('onfocus', detectDevTool);
+        window.attachEvent('onblur', detectDevTool);
+      } else {
+          setTimeout(argument.callee, 0);
+      }
+    } else {
+      window.addEventListener('load', detectDevTool);
+      window.addEventListener('resize', detectDevTool);
+      window.addEventListener('mousemove', detectDevTool);
+      window.addEventListener('focus', detectDevTool);
+      window.addEventListener('blur', detectDevTool);
+    }
+  }();
+  
