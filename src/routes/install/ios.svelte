26dac60561afcff224d965215a77ef6fd1c6cee6
Binsar Dwi Jasuma <binsarjr121@gmail.com>
2022-05-28 14:38:45 +0700

    fix: wrong class

--- src/routes/install/ios.svelte
+++ src/routes/install/ios.svelte
@@ -4,4 +4,4 @@
 	alt="GIF - Menambahkan amikom two ke home screen"
 	class="my-5 rounded-md"
 />
-<p>Bisa cek di youtube kalau masih belum paham <a href="https://www.youtube.com/watch?v=ZqlLCn7_fWk" class="bg-blue-500 hover:bg-blue-700" target="_blank">https://www.youtube.com/watch?v=ZqlLCn7_fWk</a></p>
+<p>Bisa cek di youtube kalau masih belum paham <a href="https://www.youtube.com/watch?v=ZqlLCn7_fWk" class="text-blue-500 hover:text-blue-800" target="_blank">https://www.youtube.com/watch?v=ZqlLCn7_fWk</a></p>
